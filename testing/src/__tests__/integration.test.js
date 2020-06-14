506d60280198b5d0798ae9e3e5bf2ac6c495aaae
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-14 20:48:54 +0300

    faking requests with moxios

--- testing/src/__tests__/integration.test.js
+++ testing/src/__tests__/integration.test.js
@@ -3,6 +3,19 @@ import { mount } from "enzyme";
 import moxios from "moxios";
 import Root from "Root";
 import App from "components/App";
+import moxios from "moxios";
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest("http://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "Fetched #1" }, { name: "Fetched #2" }],
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it("can fetch a list of comments and display them", () => {
   const wrapped = mount(
