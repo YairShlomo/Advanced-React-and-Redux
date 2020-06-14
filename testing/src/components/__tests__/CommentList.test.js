b3da58d8d54e8157871884f8e73460ab1275a295
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-14 18:58:05 +0300

    creates one LI per comment and show the text for each one of them. Cheerio queries.

--- testing/src/components/__tests__/CommentList.test.js
+++ testing/src/components/__tests__/CommentList.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+
 import CommentList from '../CommentList';
 import Root from 'root';
 
@@ -18,6 +19,10 @@ beforeEach(() => {
 });
 
 it('creates one LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
 });
 
-
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+});
