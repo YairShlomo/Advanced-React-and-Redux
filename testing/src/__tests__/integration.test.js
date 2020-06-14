a88cfe6de8da55037b5b5aee68196a90c8516610
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-14 21:06:48 +0300

    introducing a pause

--- testing/src/__tests__/integration.test.js
+++ testing/src/__tests__/integration.test.js
@@ -3,7 +3,6 @@ import { mount } from "enzyme";
 import moxios from "moxios";
 import Root from "Root";
 import App from "components/App";
-import moxios from "moxios";
 
 beforeEach(() => {
   moxios.install();
@@ -17,14 +16,18 @@ afterEach(() => {
   moxios.uninstall();
 });
 
-it("can fetch a list of comments and display them", () => {
+it("can fetch a list of comments and display them", (done) => {
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   );
-  //find the fetchComment buttton and click it
+
   wrapped.find(".fetch-comments").simulate("click");
-  //expect to find a list of comments
-  expect(wrapped.find("li").length).toEqual(500);
+  setTimeout(() => {
+    wrapped.update();
+    expect(wrapped.find("li").length).toEqual(2);
+    done();
+    wrapped.unmount();
+  }, 100);
 });
