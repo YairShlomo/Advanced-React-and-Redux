f85c9cd57d9701ae075bf7f7a73a2d22c33bc8df
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-14 18:28:16 +0300

    CommentList wireup

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { connect } from 'react-redux';
+class CommentList extends React.Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+  render() {
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
 
-export default () => {
-  return <div>Comment List</div>;
-};
+function mapStateToProps(state) {
+    return { comments: state.comments }
+}
+
+export default connect(mapStateToProps)(CommentList);
