41d7a352e1c6d52f28fc58d6cb05e016d92d5bb6
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-15 12:34:45 +0300

    rendering a header

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
-export default () => {
-  return (
-    <div>
-      <Route path="/posts" component={CommentBox} />
-      <Route path="/"  exact component={CommentList} />
-    </div>
-  );
-};
+export default class App extends React.Component {
+  renderButton() {}
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/posts" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
