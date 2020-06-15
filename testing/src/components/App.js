a129fcd19093a23f674754aaa8aba58c28a367f2
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-15 12:44:26 +0300

    wiring up auth state in App

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
 import { Route, Link } from "react-router-dom";
-
-export default class App extends React.Component {
-  renderButton() {}
+import { connect } from "react-redux";
+class App extends React.Component {
+  renderButton() {
+    if (this.props.auth) {
+      return <button>Sign Out</button>;
+    } else {
+      return <button>Sign In</button>;
+    }
+  }
   renderHeader() {
     return (
       <ul>
@@ -22,9 +28,16 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Route path="/posts" component={CommentBox} />
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
