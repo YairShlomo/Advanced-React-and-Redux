5039f6080c30b296339c04d2244c6a33e88a9a5d
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-15 15:15:05 +0300

    waiting for promise resoultion. hooks up async middleware

--- middleware/src/middlewares/async.js
+++ middleware/src/middlewares/async.js
@@ -1,5 +1,11 @@
+import { act } from "react-dom/test-utils";
+
 export default ({ dispatch }) => (next) => (action) => {
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
+  action.payload.then(function (response) {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
