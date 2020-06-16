29178d66ae4c7bd34478691aff16aa6a37e6d520
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-16 12:07:46 +0300

    auth init. serve setup

---
+++ auth/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+//App setup
+
+//server setup
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Sertver listening on 3090");
