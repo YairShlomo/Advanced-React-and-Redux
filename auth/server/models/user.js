f5a08e228269dee6715bbca85d1a5273704e9f6c
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-18 12:21:00 +0300

    encrypting and salting password

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,13 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 //Define our model
 const userSchema = new Schema({
-  email: {type: String, unique:true, lowercase: true },
+  email: { type: String, unique: true, lowercase: true },
   password: String,
 });
 
+//encrypt password function before a hook save for a model
+userSchema.pre("save", function (next) {
+  //get access to the user model
+  const user = this;
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+      // override plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 //create the model class
-const ModelClass = mongoose.model('user', userSchema);
+const ModelClass = mongoose.model("user", userSchema);
 //Export the model
 module.exports = ModelClass;
