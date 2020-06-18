8bee8ec4906e534a9b8804cec11258d8a66c6189
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-18 14:00:35 +0300

    use comparePasswors func. protecting Signin Route

--- auth/server/router.js
+++ auth/server/router.js
@@ -3,10 +3,12 @@ const passportService = require("./services/passport");
 const passport = require("passport");
 
 const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSIgnin = passport.authenticate("local", { session: false });
 
 module.exports = function (app) {
   app.get('/', requireAuth, function(req,res) {
     res.send({ hi:'there' })
   });
+  app.post('/signin', requireSIgnin, Authentication.signup)
   app.post("/signup", Authentication.signup);
 };
