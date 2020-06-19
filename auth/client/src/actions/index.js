b399e925583aa04d800e3a4aeb18d8ebb1d55246
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-19 13:59:26 +0300

    Signin action creator

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -23,3 +23,17 @@ export const signout = () => {
     payload: "",
   };
 };
+
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3090/signin",
+      formProps
+    );
+    localStorage.setItem("token", response.data.token);
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: "Invalid login credentials" });
+  }
+};
